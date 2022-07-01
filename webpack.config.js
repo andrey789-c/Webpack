f3499db6a96a2bb5f26a7018febd9fda1eba7936
andrey789-c <77191683+andrey789-c@users.noreply.github.com>
2022-07-01 13:27:33 +1000

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd =! isDev
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './js/main.js',
+    output: {
+        filename: `./js/${filename('js')}`,
+        path: path.resolve(__dirname, 'app'),
+        clean: true,
+        assetModuleFilename: './img/[name][ext]'
+    }, 
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'app'),
+        },
+        historyApiFallback: true,
+        open: true,
+        compress: true,
+        hot: true,
+        port: 3000,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html'),
+            filename: 'index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: `./css/style.css`
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline'},
+        ]
+    }
+}
